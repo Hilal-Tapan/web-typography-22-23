71858a2c405c5f57c65bd99144d0eca28cdc79d1
“Hilal-Tapan“ <hilalt98@hotmail.com>
2022-10-25 22:53:01 +0200

    nieuwe functies

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,22 +1,25 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-	0,
-	9,
-	12,
-	15,
-	16.5,
-	24,
-	25.8,
-	31,
-	31.1,
-	34,
-	49,
-	70,
-	75,
-	85,
-	90,
-	92,
-	93,
-	134
+	0, //sound 0
+	9, //sound1
+	12, //sound2
+	15, //sound3
+	16.5, //sound4
+	24, //sound5
+	25.8, //sound6
+	31, //sound7
+	31.1, //sound8
+	34, //sound9
+	49, //sound10
+	70, //sound11
+	75, //sound12
+	85, //sound13
+	90, //sound14
+	92, //sound15
+	93, //sound16
+	103, //sound17
+	115, //sound18
+	130, //sound19
+	134 //sound20
 ];
